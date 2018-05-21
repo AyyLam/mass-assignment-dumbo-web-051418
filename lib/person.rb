f3b99fc7bd88aab3104025200e2bbbcfd8687394
Anthony Lam <al3101@nyu.edu>
2018-05-21 18:17:00 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,9 @@
 class Person
   #your code here
-  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialization(attributes)
     attributes.each do |key, value|
-      self.class {
       self.send(("#{key}="), value)}
   end 
 end
